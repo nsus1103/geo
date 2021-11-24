0479abc607e18f1b5c48e90a925321c17609d51f
nsus1103 <nsus110315@gmail.com>
2021-11-23 19:36:31 -0500

    init

--- src/main.py
+++ src/main.py
@@ -14,11 +14,11 @@ def welcome():
 @app.get('/api/h3_index={h3_index}&h4_level={h4_level}')
 def return_value(h3_index, h4_level:int):
     assert h3.h3_is_valid(h3_index), 'INVALID H3 INDEX'
-    data = from_db.return_h3_value(h3_index)
-    s = [x[0] for x in data]
-    return {'data':{'h3_index':h3_index,'h4_level':h4_level, 'HCHO':s}}
-    # data = from_db.return_value_from_df(h3_index, h4_level)
-    # return data
+    # data = from_db.return_h3_value(h3_index)
+    # s = [x[0] for x in data]
+    # return {'data':{'h3_index':h3_index,'h4_level':h4_level, 'HCHO':s}}
+    data = from_db.return_value_from_df(h3_index, h4_level)
+    return data
 
 if __name__ == '__main__':
     uvicorn.run(app)
