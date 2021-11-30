f0d33649dce05cbc12b5ea0338b88a9e2abf8ab6
nsus1103 <33731048+nsus1103@users.noreply.github.com>
2021-11-30 13:44:30 -0500

    Delete heroku_config.py

--- src/heroku_config.py
+++
@@ -1,5 +0,0 @@
-DB_HOST = 'ec2-52-73-29-239.compute-1.amazonaws.com'
-DB_USER = 'qratmgnxeveikt'
-DB_PASS = 'fcdf7698b23f61b87e7aaa3846279985c8f647eab0c5b9bc7cb463e3721d7116'
-DB_NAME = 'dekk24mg9hjng4'
-DATABASE_URL = 'postgresql://qratmgnxeveikt:fcdf7698b23f61b87e7aaa3846279985c8f647eab0c5b9bc7cb463e3721d7116@ec2-52-73-29-239.compute-1.amazonaws.com:5432/dekk24mg9hjng4'
